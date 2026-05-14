c0ca0d425c110d7ef2acba597e2da9174cbdca72
unperrorraro <imasb2014@gmail.com>
2026-05-15 01:05:19 +0200

    RTI terminada (creo), llevo komo 1h con el puto simulador

--- prueba.s
+++ prueba.s
@@ -17,6 +17,7 @@
  
  ACR: EQU $EFFC09
  IMR: EQU $EFFC0B
+ ISR: EQU $EFFC0B
 
  MR1B: EQU $EFFC11
  MR2B: EQU $EFFC11
@@ -26,34 +27,108 @@
 
  IVR: EQU $EFFC19
 
+ IMR_2: DC.B 0
+
  ORG $1000
  RTI:
  MOVE.B #$00,IMR
- MOVEM.L D0/A0,-(A7)
+ MOVEM.L D0-D2/A2,-(A7)
+ MOVEM.L D6,-(A7)
 
- LEA COUNT,A0
- MOVE.B D0,(A0) 
+ ADD.B #$01,D3
 
- LEA BUFFER,A1
- MOVE.B 
+ MOVE.B ISR,D6
+ AND.B IMR_2,D6
+ 
+ BTST #0,D6           
+ BNE.L IN_TA  
+
+ BTST #1,D6           
+ BNE.L IN_RA         
+ 
+ BTST #4,D6           
+ BNE.L IN_TB 
+
+ BTST #5,D6  
+ BNE.L  IN_RB  
+
+ BRA.L FIN_RTI
+
+IN_RA:
+
+ MOVE.B #$00,D0
+ MOVE.B BUFF_A,D1
+ JSR ESCCAR
+ ADD.B #$01,D4
+
+ BRA.L FIN_RTI
 
 
+IN_RB:
+
+ MOVE.B #$01,D0
+ MOVE.B BUFF_B,D1
+ JSR ESCCAR
  ADD.B #$01,D5
- LEA BUFF_A,A0
- MOVE.B (A0),D0
- LEA BUFF_B,A0
- MOVE.B (A0),D0
- MOVE.B BUFF_B,D0 
- MOVEM.L (A7)+,D0/A0
- MOVE.B #$22,IMR
+
+ BRA.L FIN_RTI
+
+IN_TA:
+
+ MOVE.B #$02,D0
+ JSR LEECAR
+ CMP.L #$FFFFFFFF,D0
+ BNE HIT_IN_TA
+ AND.B #$FE,IMR_2
+ BRA.L FIN_RTI
+
+
+ HIT_IN_TA:
+ ADD.B #$01,D4
+ MOVE.B D0,BUFF_A
+
+ BRA.L FIN_RTI
+
+
+IN_TB:
+
+ MOVE #$03,D0
+ JSR LEECAR
+ CMP.L #$FFFFFFFF,D0
+ BNE HIT_TB
+ AND.B #$EF,IMR_2
+ BRA.L FIN_RTI
+
+
+
+
+
+
+HIT_TB:
+ ADD.B #$01,D5
+
+
+
+ MOVE.B D0,BUFF_B
+
+ BRA.L FIN_RTI
+
+
+
+ 
+
+
+
+
+
+
+
+ FIN_RTI: MOVEM.L (A7)+,D6
+ MOVEM.L (A7)+,D0-D2/A2
+ MOVE.B IMR_2,IMR
  RTE
 
-
-
- ORG $2000
-
- COUNT: DS.B 1
- BUFFER: DS.B 100 
+  
 
 
  ORG $3000
@@ -85,6 +160,7 @@ INIT:
 
  MOVE.B #$40,IVR
  MOVE.B #$22,IMR
+ MOVE.B #$22,IMR_2
  MOVE.B #$00,ACR
 
  MOVE.L #RTI,$100
@@ -96,21 +172,33 @@ INIT:
  RTS
 
 
+
  ORG $4000
 
  JSR INIT
- MOVE.B #$00,COUNT
  MOVE.W #$2000,SR
- EOR.L D0,D0
- EOR.L D1,D1
- ADD.B #$34,D1
- * JSR ESCCAR
- EOR.L D0,D0
- BUC:ADD.B #$0,D0
- ADD.W #$10,D2
- * JSR LEECAR
- BRA BUC
 
+ * Meter el string en el buffer de transmision A
+ LEA CADENA,A0           * A0 apunta al inicio del string
+ MOVE.W #5,D2            * contador de caracteres (longitud del string)
 
+LOOP:
+ MOVE.L #$02,D0          * buffer TX linea A
+ MOVE.B (A0)+,D1         * coger siguiente caracter
+ JSR ESCCAR
+ CMP.L #$FFFFFFFF,D0     * buffer lleno?
+ BEQ BUCLE               * si lleno, salir igualmente
+ SUBQ.W #1,D2
+ BNE LOOP
+
+ * Activar interrupcion de TX linea A en IMR_2
+ OR.B #$01,IMR_2
+ MOVE.B IMR_2,IMR
+
+BUCLE: 
+ NOP
+ BRA BUCLE
+
+CADENA: DC.B 'H','o','l','a','!'
 INCLUDE bib_aux.s
 
